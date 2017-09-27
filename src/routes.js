ee6aac18d2ba0169b6590a84eae6e34828c83c87
David Shibley <dshibley@rallydev.com>
2017-09-27 13:06:53 -0600

    react forms

--- src/routes.js
+++ src/routes.js
@@ -12,7 +12,8 @@ var routes = (
     <Route name="app" path="/" handler={require('./components/app')}>
         <DefaultRoute handler={require('./components/home')} />
         <Route name="authors" handler={require('./components/authors/authors')} />
-        {/* <Route name="authors/:authorId" handler={require('./components/authors/author')} /> */}
+        <Route name="addAuthor" path="author" handler={require('./components/authors/manageAuthor')} />
+        <Route name="manageAuthor" path="author/:id" handler={require('./components/authors/manageAuthor')} />
         <Route name="about" handler={require('./components/about/about')} />
         <NotFoundRoute handler={require('./components/notFound')} />
         <Redirect from="about-us" to="about" />
